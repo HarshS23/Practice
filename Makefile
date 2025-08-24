935a7fe136223e4604944ee6c1693faaca91e23a
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-08-23 18:58:44 -0600

    Inital commit with local files

---
+++ Makefile
@@ -0,0 +1,7 @@
+# # format 
+# # Target: dependencies 
+# #	action	
+# #
+
+CC = gcc 
+CFLAGS = -Wall -Wextra -std=c99 -O2 
---
+++ insertion_sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(void){
+
+    int array[] = {1,5,8,4,9,2,5,83,23,8980,788082134,-1};
+
+    for(int i = 0; i < 13; i++){
+        printf("this is the array: %d\n ", i);
+    }
+
+
+
+};
