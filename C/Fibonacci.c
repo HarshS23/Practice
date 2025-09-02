dbf2a5be8b5fc7185fd56334aecc6cdeead6cc97
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-09-02 12:38:27 -0600

    C

--- C/Fibonacci.c
+++ C/Fibonacci.c
@@ -11,12 +11,39 @@ int fib1(int n){
 
 }
 
+int fib2(int n){
+    int F[] = {0,1};
+
+    for(int i = 3 ; i < n+1; i++){ 
+        F[i] = F[i-2] + F[i - 3];
+    }
+    return F[n + 1];
+
+
+}
+
+int fib3(int n){
+    if (n = 0) return 0; 
+    int x = 0;
+    int y = 1;
+
+    for(unsigned int k = 2; k < n; k++){
+        k = x + y; 
+        x = y; 
+        y = k; 
+    }
+    return y;
+
+}
+
 
 int main(){
     int y;
     printf("Input a value: ");
     scanf(" %d", &y);
     int x = fib1(y);
+    int z = fib2(y);
 
-    printf("FIB1 value : %d\n", x);
+    printf("FIB1 value: %d\n", x);
+    Printf("FIB2 value: %d\n", z);
 }
